62356d4b9731ec9f337c5bb53ffd243ecf21bec8
Matthew Brisebois <matthew@webheroes.ca>
2021-09-20 16:40:47 -0600

    Support Holochain v0.0.104

--- default.nix
+++ default.nix
@@ -11,14 +11,14 @@ let
     };
 
     holochainVersionId = "custom";
-    holochainVersion = { # v0.0.103
-      rev = "f3d17d993ad8d988402cc01d73a0095484efbabb"; # Aug 17, 2021
-      sha256 = "1z0y1bl1j2cfv4cgr4k7y0pxnkbiv5c0xv89y8dqnr32vli3bld7";
-      cargoSha256 = "1rf8vg832qyymw0a4x247g0iikk6kswkllfrd5fqdr0qgf9prc31";
+    holochainVersion = { # v0.0.104
+      rev = "d003eb7a45f1d7125c4701332202761721793d68"; # Aug 25, 2021
+      sha256 = "0qxadszm2a7807w49kfbj7cx6lr31qryxcyd2inyv7q5j7qbanf2";
+      cargoSha256 = "129wicin99kmxb2qwhll8f4q78gviyp73hrkm6klpkql6810y3jy";
 
-      lairKeystoreHashes = { # v0.0.3
-        sha256 = "1jiz9y1d4ybh33h1ly24s7knsqyqjagsn1gzqbj1ngl22y5v3aqh";
-        cargoSha256 = "0agykcl7ysikssfwkjgb3hfw6xl0slzy38prc4rnzvagm5wd1jjv";
+      lairKeystoreHashes = { # v0.0.4
+        sha256 = "0khg5w5fgdp1sg22vqyzsb2ri7znbxiwl7vr2zx6bwn744wy2cyv";
+        cargoSha256 = "1lm8vrxh7fw7gcir9lq85frfd0rdcca9p7883nikjfbn21ac4sn4";
       };
 
       bins = {
