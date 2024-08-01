f999ae85f30b79d655faab3452161b66e4ce2e71
Perry Kundert <perry@dominionrnd.com>
2024-08-01 09:11:10 -0600

    Attempt to make nix-overlay support more architectures

--- flake.nix
+++ flake.nix
@@ -5,35 +5,39 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import ./pkgs.nix {
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit system;
-      };
-    in
-    {
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            holochain_0-4
-            lair-keystore_0-4-5
-            hc_0-4
-
-            rustup
-            cargo
-            rustc
-
-            nodejs_22
-          ];
-
-          shellHook = ''
-            export PS1="\[\e[1;32m\](flake-env)\[\e[0m\] \[\e[1;34m\]\u@\h:\w\[\e[0m\]$ "
-            rustup default stable
-            rustup target add wasm32-unknown-unknown
-          '';
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import ./pkgs.nix {
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit system;
         };
-      };
-    };
+	#debugLog = builtins.trace "pkgs output: ${builtins.toJSON pkgs}";
+        debugLog = builtins.trace "Available attributes: ${builtins.toString (builtins.attrNames pkgs)}";
+      in
+      {
+        #devShell = {
+        #  default = pkgs.mkShell {
+        devShell = debugLog (pkgs.mkShell {
+            buildInputs = with pkgs; [
+              holochain_0-4
+              lair-keystore_0-4-5
+              hc_0-4
+    
+              rustup
+              cargo
+              rustc
+    
+              nodejs_22
+            ];
+    
+            shellHook = ''
+              export PS1="\[\e[1;32m\](flake-env)\[\e[0m\] \[\e[1;34m\]\u@\h:\w\[\e[0m\]$ "
+              rustup target add wasm32-unknown-unknown
+            '';
+        });
+        #  };
+        #};
+      }
+    );
 }
--- pkgs.nix
+++ pkgs.nix
@@ -1,10 +1,11 @@
 { pkgs, system }:
 
 import (pkgs.fetchFromGitHub {
-  owner = "spartan-holochain-counsel";
+  #owner = "spartan-holochain-counsel";
+  owner = "pjkundert";
   repo = "nix-overlay";
-  rev = "60304b93954c201cb4e1f75eb17295789e1bf967";
-  sha256 = "qgewX2TbZdlPRkGPvyvUmo7gDj4122JGypi1uwdXcTg=";
+  rev = "dbd4d2b28d861a9536a89d7012a730ebbca28676"; # feature-arch
+  sha256 = "sha256-xeE4gFEssB17Hr69JepP2k5J8uwrSQdpMtvn99Pd7oQ=";
 }) {
   inherit pkgs;
   inherit system;
